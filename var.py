ce707669e224f0c13ecc80928affa860d23d617f
Prabin <Prabin.ktm00@gmail.com>
2023-03-31 17:25:58 +0530

    var File Add

---
+++ var.py
@@ -0,0 +1,13 @@
+firstname = "Prabin"
+lastname = "Sharma"
+address = "Noida"
+company = "Teraidatalabs Pvt Ltd"
+role = "Cloud/Domino Administrator"
+phone_number = 8826490937
+
+print(firstname)
+print(lastname)
+print(address)
+print(company)
+print(role)
+print(phone_number)
